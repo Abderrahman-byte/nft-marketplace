0a7cd24a70df50ae4534c2340535f98989e10ec3
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-13 10:20:31 +0000

    Initialize spring boot project

---
+++ src/main/java/org/merchantech/nftproject/NftProjectApplication.java
@@ -0,0 +1,13 @@
+package org.merchantech.nftproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftProjectApplication.class, args);
+	}
+
+}
