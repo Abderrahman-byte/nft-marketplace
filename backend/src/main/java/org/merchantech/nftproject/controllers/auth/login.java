b632357d9bbbd286bd5c29fe51565664cbabde48
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-13 21:42:26 +0000

    feat: set error message

--- backend/src/main/java/org/merchantech/nftproject/controllers/auth/login.java
+++ backend/src/main/java/org/merchantech/nftproject/controllers/auth/login.java
@@ -1,5 +1,9 @@
 package org.merchantech.nftproject.controllers.auth;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/${api.version}/login")
 public class login {
     @GetMapping
-    String handleGetRequest () {
-        return "This path is not implemented";
+    Map<Object, Object> handleGetRequest () {
+        Map<Object, Object> data = new HashMap<>();
+        data.put("errors", List.of("login page is not implemented"));
+        data.put("ok", false);
+        
+        return data;
     }
 }
