efa6f54d380ad875ec42f9ff7ce289820fc99129
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-14 09:46:34 +0000

    feat: add empty Register and login controllers

--- backend/src/main/java/org/merchantech/nftproject/controllers/auth/LoginController.java
+++ backend/src/main/java/org/merchantech/nftproject/controllers/auth/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/${api.version}/auth/register")
-public class RegisterController {
+@RequestMapping("/api/${api.version}/auth/login")
+public class LoginController {
     
 }
