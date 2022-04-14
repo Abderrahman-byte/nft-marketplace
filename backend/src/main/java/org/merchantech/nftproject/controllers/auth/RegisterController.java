df4c51c19aaeab436f00349c4ec3507b9e2651a0
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-14 13:57:25 +0000

    feat : create new account if form data is valid

--- backend/src/main/java/org/merchantech/nftproject/controllers/auth/RegisterController.java
+++ backend/src/main/java/org/merchantech/nftproject/controllers/auth/RegisterController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.merchantech.nftproject.model.dao.AccountDAO;
 import org.merchantech.nftproject.validation.RegisterFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
@@ -17,6 +19,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// TODO : Send email verification
 
 @RestController
 @RequestMapping("/api/${api.version}/auth/register")
@@ -27,12 +31,15 @@ public class RegisterController {
     @Autowired
     private MessageSource messageSource;
 
+    @Autowired
+    private AccountDAO accountDAO;
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     Map<String, Object> handlePostRequest (@RequestBody Map<String, Object> data) {
         Map<String, Object> response = new HashMap<>();
         MapBindingResult errors = new MapBindingResult(data, "register");
 
-        // Check received data
+        response.put("ok", true);
         formValidator.validate(data, errors);
 
         if (errors.hasErrors()) {
@@ -41,8 +48,16 @@ public class RegisterController {
             return response;
         }
 
-        // Create user account
-        // send email verification
+        try {
+            accountDAO.insertAccount(data);
+        } catch (DataIntegrityViolationException ex) {
+            response.put("ok", false);
+            response.put("errors", this.getTransactionsErrors(ex));
+        } catch (Exception ex) {
+            response.put("ok", false);
+            response.put("errors", List.of("unkown_errors"));
+        }
+
 
         return response;
     }
@@ -57,5 +72,19 @@ public class RegisterController {
         }
 
         return errorsString;
+    }
+
+    private List<String> getTransactionsErrors (DataIntegrityViolationException ex) {
+        String message = ex.getMessage();
+
+        if (message.contains("email")) {
+            return List.of("An account with same email already exists.");
+        } 
+        
+        if (message.contains("username")) {
+            return List.of("An account with same username already exists.");
+        }
+
+        return List.of("unkown_error");
     }
 }
