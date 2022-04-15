7bc7036f893feec8770eaf65842773e0b6043eac
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-15 20:55:33 +0000

    feat: filter expired sessions

--- backend/src/main/java/org/merchantech/nftproject/model/dao/SessionDAO.java
+++ backend/src/main/java/org/merchantech/nftproject/model/dao/SessionDAO.java
@@ -1,7 +1,13 @@
 package org.merchantech.nftproject.model.dao;
 
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.merchantech.nftproject.model.bo.Session;
 import org.springframework.stereotype.Repository;
@@ -18,8 +24,22 @@ public class SessionDAO {
         entityManager.persist(s);
     }
     
-    // TODO : filter Expired session
     public Session getSessionById (String sid) {
-        return entityManager.find(Session.class, sid);
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Session> cq = cb.createQuery(Session.class);
+        Root<Session> root = cq.from(Session.class);
+
+        cq.select(root).where(
+            cb.and(
+                cb.equal(root.get("id"), sid),
+                cb.greaterThan(root.get("expires"), Calendar.getInstance())
+            )
+        );
+
+        try {
+            return (Session)entityManager.createQuery(cq).getSingleResult();
+        } catch (NoResultException ex) {
+            return null;
+        }
     }
 }
