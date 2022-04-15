1ebec611e0a34d464251b987a8f687b8f448e607
Fadwa <fadwaelalaoui0@gmail.com>
2022-04-15 09:41:22 +0000

    logthird3commit

--- backend/src/main/java/org/merchantech/nftproject/controllers/auth/LoginController.java
+++ backend/src/main/java/org/merchantech/nftproject/controllers/auth/LoginController.java
@@ -1,6 +1,9 @@
 package org.merchantech.nftproject.controllers.auth;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.merchantech.nftproject.errors.ApiError;
@@ -11,15 +14,12 @@ import org.merchantech.nftproject.utils.PasswordHasher;
 import org.merchantech.nftproject.validation.LoginFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-<<<<<<< HEAD
 import org.springframework.http.MediaType;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.MapBindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
-=======
 import org.springframework.validation.MapBindingResult;
->>>>>>> e56b5b03b516b06adcb010a50f5c8df2f746f064
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,27 +37,20 @@ public class LoginController {
 	@Autowired
 	private MessageSource messageSource;
 
-<<<<<<< HEAD
+
 	    @Autowired
 	    private AccountDAO accountDAO;
 	    
 	    private Account temp = new Account();
 	   
    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	Map<String, Object> handlePostRequest(@RequestBody Map<String, Object> data){
-=======
-	@Autowired
-	private AccountDAO accountDAO;
-	
-	Map<String, Object> handlePostRequest(@RequestBody Map<String, Object> data) throws ApiError {
->>>>>>> e56b5b03b516b06adcb010a50f5c8df2f746f064
+	Map<String, Object> handlePostRequest(@RequestBody Map<String, Object> data) {
 		Map<String, Object> response = new HashMap<>();
 		MapBindingResult errors = new MapBindingResult(data, "login");
 
 		response.put("success", true);
 		formValidator.validate(data, errors);
-		
-<<<<<<< HEAD
+
 		  if (errors.hasErrors()) {
 	            response.put("errors", this.getErrorList(errors));
 	            response.put("ok", false);
@@ -77,22 +70,9 @@ public class LoginController {
 	        for (FieldError error: fieldErrors) {
 	            errorsString.add(messageSource.getMessage(error, Locale.US));
 	        }
-	        return errorsString;
-	    }
-	 
-}
-
-
-
-
-=======
-		if (errors.hasErrors()) throw new ValidationError(errors, messageSource);
+	       return errorsString;
+	    
 		
-		Account account = accountDAO.getAccountByUsername(String.valueOf(data.get("username")));
-		
-		// if(temp!=null || temp.getPassword())
-
-		return response;
 	}
 }
->>>>>>> e56b5b03b516b06adcb010a50f5c8df2f746f064
+
