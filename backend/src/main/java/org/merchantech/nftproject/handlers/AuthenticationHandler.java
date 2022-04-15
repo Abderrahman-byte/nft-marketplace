a06afded4f36702e85fc90b214f30256f151b3d8
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-15 20:38:51 +0000

    feat : implement intercepter that check if user is authenticated

---
+++ backend/src/main/java/org/merchantech/nftproject/handlers/AuthenticationHandler.java
@@ -0,0 +1,55 @@
+package org.merchantech.nftproject.handlers;
+
+import java.util.Map;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.merchantech.nftproject.model.bo.Account;
+import org.merchantech.nftproject.model.bo.Session;
+import org.merchantech.nftproject.model.dao.AccountDAO;
+import org.merchantech.nftproject.model.dao.SessionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthenticationHandler implements HandlerInterceptor {
+    @Autowired
+    private SessionDAO sessionDAO;
+
+    @Autowired
+    private AccountDAO accountDAO;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Cookie cookies[] = request.getCookies();
+
+        if (cookies == null) return true;
+
+        for (Cookie cookie : cookies) {
+            if (!cookie.getName().equals("sessionId")) continue;
+
+            String sessionId = cookie.getValue();
+
+            if (sessionId == null || sessionId.equals("")) break;
+
+            Session session = sessionDAO.getSessionById(sessionId);
+            
+            if (session == null || session.getPayload() == null || !session.getPayload().containsKey("currentUser")) break;
+            
+            Map<String, Object> payload = session.getPayload();
+            String accountId = (String)payload.get("currentUser");
+            Account account = accountDAO.getAccountById(accountId);
+            
+            if (account == null) break;
+            
+            request.setAttribute("account", account);
+            request.setAttribute("session", session);
+            request.setAttribute("accountId", accountId);
+
+            break;
+        }
+
+        return true;
+    }
+}
