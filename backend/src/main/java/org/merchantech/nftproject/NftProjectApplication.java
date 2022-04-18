55a518200fe54138da590f0a009680ab05f4346b
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-18 10:59:09 +0000

    fix: remove useless imports

--- backend/src/main/java/org/merchantech/nftproject/NftProjectApplication.java
+++ backend/src/main/java/org/merchantech/nftproject/NftProjectApplication.java
@@ -1,10 +1,5 @@
 package org.merchantech.nftproject;
 
-import org.merchantech.nftproject.model.bo.Account;
-import org.merchantech.nftproject.model.dao.AccountDAO;
-import org.merchantech.nftproject.model.dao.NFTCollectionDAO;
-import org.merchantech.nftproject.model.dao.NFTDAO;
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
