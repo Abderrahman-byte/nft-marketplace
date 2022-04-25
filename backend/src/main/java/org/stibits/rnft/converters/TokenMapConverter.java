58674d187d8386ce40f0c211a9d5a0e1e0cf9c2b
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-25 14:11:14 +0000

    feat : convert NFT to json map

---
+++ backend/src/main/java/org/stibits/rnft/converters/TokenMapConverter.java
@@ -0,0 +1,60 @@
+package org.stibits.rnft.converters;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.stibits.rnft.model.bo.Account;
+import org.stibits.rnft.model.bo.NFToken;
+
+@Component
+public class TokenMapConverter implements Converter<NFToken, Map<String, Object>> {
+    @Autowired
+    public ProfileDetailsConverter profileDetailsConverter;
+
+    public List<Map<String, Object>> convertList (List<NFToken> nfts, Account account) {
+        return nfts.stream().map(nft -> this.convert(nft, account)).toList();
+    }
+
+    public List<Map<String, Object>> convertList (List<NFToken> nfts) {
+        return nfts.stream().map(nft -> this.convert(nft)).toList();
+    }
+
+    @Override
+    public Map<String, Object> convert(NFToken source) {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("id", source.getId());
+        data.put("title", source.getTitle());
+        data.put("previewUrl", source.getPreviewUrl());
+        data.put("price", source.getPrice());
+        data.put("description", source.getDescription());
+        data.put("likesCount", source.getLikes().size());
+
+        if (source.getCreator().getProfile() != null) {
+            data.put("creator", profileDetailsConverter.convert(source.getCreator().getProfile()));
+        } else {
+            data.put("creatorId", source.getCreator().getId());
+        }
+        
+        if (source.getOwner().getProfile() != null) {
+            data.put("owner", profileDetailsConverter.convert(source.getOwner().getProfile()));
+        } else {
+            data.put("ownerId", source.getOwner().getId());
+        }
+
+        return data;
+    }
+
+    public Map<String, Object> convert(NFToken source , Account account) {
+        Map<String, Object> data = this.convert(source);
+
+        if (account != null)
+            data.put("liked", source.getLikes().stream().filter(like -> like.getAccountId().equals(account.getId())).count() > 0);
+
+        return data;
+    }
+}
