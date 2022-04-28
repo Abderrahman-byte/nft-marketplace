20e09a0c5a0b1d85b7770f36d0776a3d930025d8
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-04-28 13:55:22 +0000

    feat: update context cover

--- frontend/src/pages/Profile.page.jsx
+++ frontend/src/pages/Profile.page.jsx
@@ -1,47 +1,29 @@
-import React, {useEffect, useState}from "react";
+import React, { useContext, useEffect, useState } from 'react'
 
-import { getProfile } from "@Utils/api";
-import ProfileCover from "@/components/ProfileCover";
-import ProfileInfoCard from "@/components/ProfileInfoCard";
+import { getProfile } from '@Utils/api'
+import ProfileCover from '@/components/ProfileCover'
+import ProfileInfoCard from '@/components/ProfileInfoCard'
+
+import ProfileNavbar from '@/components/ProfileNavbar'
+import { AuthContext } from '@/context/AuthContext'
 
 import '@Styles/ProfilePage.css'
-import ProfileNavbar from "@/components/ProfileNavbar";
 
 const ProfilePage = () => {
-    const [profile, setprofile]= useState({});
+	const { account:profile } = useContext(AuthContext)
 
-    const getProfileData = async () => {       
-        const profile = await getProfile();
-     
-        setprofile(profile)
-         console.log("profile page")
-    }
-
-    useEffect(()=>{
-        getProfileData();
-    },[])
-
-    return (
-        <div className="ProfilePage">
-            <ProfileCover allowUpdate profile={profile} />
-            <div className="container">
-                <ProfileInfoCard profile={profile} />
-                <div className="profile-pages">
-                    <ProfileNavbar />
-                    <div className="profile-content">
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-
-
-
-
+	return (
+		<div className='ProfilePage'>
+			<ProfileCover allowUpdate profile={profile} />
+			<div className='container'>
+				<ProfileInfoCard profile={profile} />
+				<div className='profile-pages'>
+					<ProfileNavbar />
+					<div className='profile-content'></div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default ProfilePage;
-
-
-
+export default ProfilePage
