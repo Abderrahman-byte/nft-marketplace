a8c88b96f600942e579af911ef2a4f2ff9032310
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-05-03 15:52:20 +0000

    feat: Implement update preview card on input

--- frontend/src/pages/UploadSingle.page.jsx
+++ frontend/src/pages/UploadSingle.page.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import CreateSingleItemForm from '@Components/CreateTokenForm'
 
 import '@Styles/UploadItem.css'
 import TokenCard from '@/components/TokenCard'
+import { AuthContext } from '@/context/AuthContext'
 
 const UploadSinglePage = () => {
+	const { account } = useContext(AuthContext)
+
+	const [tokenData, setTokenData] = useState({ price: 0.0 })
+
+	const onUpdateCallback = (data) => {
+		setTokenData({...tokenData, ...data})
+	}
+
+	useEffect(() => {
+		setTokenData({ ...tokenData, owner: account, creator: account })
+	}, [account])
+
 	return (
 		<div className='UploadSinglePage UploadItem'>
 			<div className='container'>
@@ -15,11 +28,11 @@ const UploadSinglePage = () => {
                         <h2>Create Single collectible</h2>
                         <Link className='btn btn-white' to='#'>Switch to multiple</Link>
                     </div>
-                    <CreateSingleItemForm />
+                    <CreateSingleItemForm onUpdateCallback={onUpdateCallback} />
 				</div>
 				<div className='preview'>
 					<h2>Preview</h2>
-					<TokenCard />
+					<TokenCard {...tokenData} />
 					<button className='btn clean-all-btn'>
 						<i className='timer-icon'></i>
 						<span>Clean all</span>
