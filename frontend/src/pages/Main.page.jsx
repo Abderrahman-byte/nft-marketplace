999dbd9300518857390c40d0f46407183e72ef01
Abderrahman-byte <el.abderrahman00@gmail.com>
2022-05-05 13:49:33 +0000

    feat : add most popular token to main page

--- frontend/src/pages/Main.page.jsx
+++ frontend/src/pages/Main.page.jsx
@@ -6,16 +6,19 @@ import { getCollectionsList, getTokens } from '@/utils/api'
 
 import '@Styles/MainPage.css'
 import CollectionCard from '@/components/CollectionCard'
+import MostPopularToken from '@/components/MostPopularToken'
 
 const MainPage = () => {
     const [popularTokens, setPopularTokens] = useState([])
     const [collections, setCollections] = useState([])
+    const [trendToken, setTrendToken] = useState({})
     const [childrenWidth, setChildrenWidth] = useState(100)
     const [childrenGap, setChildrenGap] = useState(0)
 
     const fetchData = async () => {
         const tokens = await getTokens('LIKES', 100000, 100)
         setPopularTokens(tokens)
+        setTrendToken(tokens.length > 0 ? tokens[0] : null)
 
         const collectionsList = await getCollectionsList(3, true)
         setCollections(collectionsList)
@@ -27,6 +30,10 @@ const MainPage = () => {
 
     return (
         <div className='page MainPage'>
+            <div>
+                <MostPopularToken {...trendToken} />
+            </div>
+
             <Carossel title='Popular' childrenWidthPercentage={22.85} gapPersentage={2.84} setGap={setChildrenGap} setChildrenWidth={setChildrenWidth}>
                 {popularTokens.map((token, i) => <TokenCard style={{ minWidth: childrenWidth + 'px', marginRight: childrenGap + 'px'}} key={i} likable link {...token} />)}
             </Carossel>
